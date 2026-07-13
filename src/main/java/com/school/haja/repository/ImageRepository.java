580fca1f667861d6b0240047717ec1dbd6f996c8
haja171106 <hei.haja.2@gmail.com>
2026-07-13 09:54:36 +0300

    fix: fix bug in image repository

--- src/main/java/com/school/haja/repository/ImageRepository.java
+++ src/main/java/com/school/haja/repository/ImageRepository.java
@@ -1,6 +1,7 @@
 package com.school.haja.repository;
 
 import com.school.haja.repository.model.JImage;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ImageRepository extends JpaRepository<JImage, Long> {}
+public interface ImageRepository extends JpaRepository<JImage, UUID> {}
